b2ae124ab0a5f71627cac0d44584d499ae8df104
Elin Johansson <elinmariejohansson@gmail.com>
2018-12-14 11:59:51 +0100

    no message

---
+++ BeforeCheckpoint03/BeforeCheckpoint03/MultipleArguments.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MethodsAndLists.Test
+{
+    internal class MultipleArguments
+    {
+        internal object ComputeSequenceSumOrProduct(int v1, bool v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object ComputeSequence(int v, object sum)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Checkpoint3/Checkpoint3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Checkpoint3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Level1();
+        }
+
+        private static void Level1()
+        {
+            List<int> list = new List<int> { 2, 8, 3, 11 };
+            List<int> newList = MultiplyBy100AndAdd3(list);
+            DisplayResults(list, newList);
+        }
+
+        private static List<int> MultiplyBy100AndAdd3(List<int> list)
+        {
+            List<int> newList = new List<int>();
+
+            foreach (int number in list)
+            {
+                int newNumber = (number * 100) + 3;
+                newList.Add(newNumber);
+            }
+
+            return newList;
+        }
+
+        private static void DisplayResults(List<int> list, List<int> newList)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("LISTAN");
+            Console.ResetColor();
+            Console.WriteLine(String.Join(',', list));
+
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("MULTIPLICERA MED 100 OCH LÄGG TILL TRE");
+            Console.ResetColor();
+            Console.WriteLine(String.Join(',', newList));
+
+            Console.WriteLine();
+
+        }
+    }
+}
